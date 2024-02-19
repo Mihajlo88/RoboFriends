02e75280fd6509e7c9cf1765770d685df6a93080
Mihajlo Radojkovic <mradojkovic@outlook.com>
2024-02-19 13:13:20 -0500

    adding changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// the app gets rendered here -- APP.JS is rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
