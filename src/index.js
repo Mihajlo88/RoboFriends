e05af8d0202cb431ab0000baf7599dbb3a36c2c3
Mihajlo Radojkovic <mradojkovic@outlook.com>
2024-02-20 10:30:25 -0500

    commited

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 // the app gets rendered here -- APP.JS is rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <h1>Hello World</h1> */}
-    <Hello greeting={"Hello React Engineer"}/>
+    <div>
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
+    <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email} />
+    <Card id={robots[4].id} name={robots[4].name} username={robots[4].username} email={robots[4].email} />
+    </div>
   </React.StrictMode>
 );
 
---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sara Radojkovic',
+    username: 'Stimpi',
+    email: 'sara_radojkovic@outlook.com'
+  },
+  {
+    id: 2,
+    name: 'Kosta Radojkovic',
+    username: 'Djavo',
+    email: 'kosta_radojkovic@outlook.com'
+  },
+  {
+    id: 3,
+    name: 'Dusan Radojkovic',
+    username: 'Debeli',
+    email: 'dusan_radojkovic@outlook.com'
+  },
+  {
+    id: 4,
+    name: 'Mihajlo Radojkovic',
+    username: 'Sexy',
+    email: 'mradojkovic@outlook.com'
+  },
+  {
+    id: 5,
+    name: 'Oskar',
+    username: 'Dzukac',
+    email: 'oskar@outlook.com'
+  } 
+];
