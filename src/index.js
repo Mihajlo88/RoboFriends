dec76968607429eeab32d1a9cbf74f0a39751d41
Mihajlo Radojkovic <mradojkovic@outlook.com>
2024-02-20 12:40:43 -0500

    commited changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card.js';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
@@ -9,14 +9,7 @@ import { robots } from './robots';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <h1>Hello World</h1> */}
-    <div>
-    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email} />
-    <Card id={robots[4].id} name={robots[4].name} username={robots[4].username} email={robots[4].email} />
-    </div>
+   <CardList robots={ robots }/>
   </React.StrictMode>
 );
 
