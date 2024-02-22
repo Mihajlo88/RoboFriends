87ade717ace50a970511ea53a6ad7c5efc4f9358
Mihajlo Radojkovic <mradojkovic@outlook.com>
2024-02-22 13:00:02 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
-
+import './App.css';
 // props(properties) are things that come out of state
 // state describes the app - it's an object that describes our app
 
@@ -30,7 +30,7 @@ class App extends Component {
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots} />
             </div>
