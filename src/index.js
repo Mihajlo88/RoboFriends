1ea3ab420d8d1bee06af10e145767e3ac7c248a6
Mihajlo Radojkovic <mradojkovic@outlook.com>
2024-02-22 20:41:32 -0500

    commited

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
+// import { robots } from './robots';
 // the app gets rendered here -- APP.JS is rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <App robots={robots}/>
+   <App />
   </React.StrictMode>
 );
 
